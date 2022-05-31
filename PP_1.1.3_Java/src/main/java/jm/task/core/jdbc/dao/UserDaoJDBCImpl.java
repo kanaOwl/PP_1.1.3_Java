97afd7d782839cdc7190c5afe80cc72911743698
kanaOwl <100942036+kanaOwl@users.noreply.github.com>
2022-05-31 18:45:45 +0300

    Add files via upload

---
+++ PP_1.1.3_Java/src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -0,0 +1,82 @@
+package jm.task.core.jdbc.dao;
+
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDaoJDBCImpl implements UserDao {
+    public UserDaoJDBCImpl() {
+    }
+    @Override
+    public void createUsersTable() {
+        try (Connection connection = Util.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("CREATE TABLE IF NOT EXISTS Users (id INT PRIMARY KEY AUTO_INCREMENT, " +
+                     "name VARCHAR(25), lastName VARCHAR(30), age INT);")) {
+            preparedStatement.executeUpdate();
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+    }
+    @Override
+    public void dropUsersTable() {
+        try (Connection connection = Util.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("DROP TABLE IF EXISTS Users")) {
+            preparedStatement.executeUpdate();
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+    }
+    @Override
+    public void saveUser(String name, String lastName, byte age) {
+        try (Connection connection = Util.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("INSERT Users (name, lastName, age) VALUES " +
+                     "('" + name + "', '" + lastName + "', " + (age + ")"))) {
+            System.out.println("Пользователь с именем " + name + "добавлен!");
+            preparedStatement.executeUpdate();
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+    }
+    @Override
+    public void removeUserById(long id) {
+        try (Connection connection = Util.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM Users WHERE id = "  + id + ";")) {
+            System.out.println("Пользователь " + id + " удалён!");
+            preparedStatement.executeUpdate();
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+    }
+    @Override
+    public List<User> getAllUsers() {
+        List<User> users = new ArrayList<>();
+        try (Connection connection = Util.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM Users");
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+            while (resultSet.next()) {
+                String name = resultSet.getString(2);
+                String lastName = resultSet.getString(3);
+                byte age = resultSet.getByte(4);
+                users.add(new User(name, lastName, age));
+            }
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+        return users;
+    }
+    @Override
+    public void cleanUsersTable() {
+        try (Connection connection = Util.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement("TRUNCATE TABLE Users")) {
+            preparedStatement.executeUpdate();
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+    }
+}
---
+++ PP_1.1.3_Java/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,26 @@
+package jm.task.core.jdbc.util;
+
+import java.lang.reflect.InvocationTargetException;
+import java.sql.*;
+
+public class Util {
+
+    private static final String hostName = "jdbc:mysql://localhost:3306/test";
+    private static final String userName = "root";
+    private static final String password = "Ft91Q#hg543%z0";
+    public static Connection connection;
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+            connection = DriverManager.getConnection(hostName, userName, password);
+        } catch (SQLException | ClassNotFoundException | NoSuchMethodException | InstantiationException |
+                 IllegalAccessException | InvocationTargetException ex) {
+            ex.printStackTrace();
+        }
+        return connection;
+    }
+}
+
+
+
