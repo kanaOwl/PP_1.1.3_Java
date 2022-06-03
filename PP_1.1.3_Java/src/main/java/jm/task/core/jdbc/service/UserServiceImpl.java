80d5fd206658b677275a21384ff9ee2a4c07ca1a
kanaOwl <100942036+kanaOwl@users.noreply.github.com>
2022-06-03 16:36:27 +0300

    Update UserServiceImpl.java

--- PP_1.1.3_Java/src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ PP_1.1.3_Java/src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -5,27 +5,34 @@ import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 import java.util.List;
 public class UserServiceImpl implements UserService {
-    UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+    
+    private UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+    
     @Override
     public void createUsersTable() {
         userDaoJDBC.createUsersTable();
     }
+    
     @Override
     public void dropUsersTable() {
         userDaoJDBC.dropUsersTable();
     }
+   
     @Override
     public void saveUser(String name, String lastName, byte age) {
         userDaoJDBC.saveUser(name, lastName, age);
     }
+    
     @Override
     public void removeUserById(long id) {
         userDaoJDBC.removeUserById(id);
     }
+    
     @Override
     public List<User> getAllUsers() {
         return userDaoJDBC.getAllUsers();
     }
+    
     @Override
     public void cleanUsersTable() {
         userDaoJDBC.cleanUsersTable();
